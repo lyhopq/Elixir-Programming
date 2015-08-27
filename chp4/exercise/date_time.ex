89bac029c7e913eda5af08c77e966b236d08d2b6
lyhopq <lyhopq@163.com>
2015-08-27 16:46:15 +0800

    use calendar

--- chp4/exercise/date_time.ex
+++ chp4/exercise/date_time.ex
@@ -10,6 +10,14 @@ defmodule DateTime do
     (mega_secs1 - mega_secs2) * 1_000_000 + (secs1 - secs2) + (micro_secs1 - micro_secs2) * 0.000001
   end
 
+  # defp time_difference(t1, t2) do
+  #   {days, {hours, minutes, seconds}} = :calendar.time_difference(
+  #     :calendar.now_to_local_time(t2),
+  #     :calendar.now_to_local_time(t2)
+  #   )
+  #   (days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60) + seconds
+  # end
+
   def my_date_string do
     {year, month, day} = :erlang.date
     {hour, minute, second} = :erlang.time
